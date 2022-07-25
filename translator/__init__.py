d0322e6931502d8d96ec5e6b9ddc0e72565e766e
Sean Russell <sean.russell@ucd.ie>
2022-07-25 13:48:59 +0100

    added

--- translator/__init__.py
+++ translator/__init__.py
@@ -6,6 +6,14 @@ from flask import Flask, jsonify, request, render_template, url_for
 from tempfile import mkdtemp
 from flask_caching import Cache
 
+from pylti1p3.contrib.flask import FlaskOIDCLogin, FlaskMessageLaunch, FlaskRequest, FlaskCacheDataStorage
+from pylti1p3.deep_link_resource import DeepLinkResource
+from pylti1p3.grade import Grade
+from pylti1p3.lineitem import LineItem
+from pylti1p3.tool_config import ToolConfJsonFile
+from pylti1p3.registration import Registration
+
+
 app = Flask(__name__)
 
 preface = ""
@@ -27,6 +35,28 @@ config = {
 
 app.config.from_mapping(config)
 cache = Cache(app)
+
+def get_lti_config_path():
+    return os.path.join(app.root_path, 'translator', 'config', 'tool.json')
+
+
+def get_launch_data_storage():
+    return FlaskCacheDataStorage(cache)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    tool_conf = ToolConfJsonFile(get_lti_config_path())
+    launch_data_storage = get_launch_data_storage()
+
+    flask_request = FlaskRequest()
+    target_link_uri = flask_request.get_param('target_link_uri')
+    if not target_link_uri:
+        raise Exception('Missing "target_link_uri" param')
+
+    oidc_login = FlaskOIDCLogin(flask_request, tool_conf, launch_data_storage=launch_data_storage)
+    return oidc_login.enable_check_cookies().redirect(target_link_uri)
+
 
 @app.route('/init/', methods=['GET'])
 def main_page():
