63bbfd0701615844263d6afb3b4ae8aa5205ccd5
Sean <sean.russell@ucd.ie>
2022-07-25 12:47:21 +0100

    added route

--- translator/__init__.py
+++ translator/__init__.py
@@ -5,3 +5,10 @@ import pprint
 from flask import Flask, jsonify, request, render_template, url_for
 
 app = Flask(__name__)
+
+preface = ""
+
+
+@app.route('/quiz/', methods=['GET'])
+def main_page():
+    return render_template('main.html', preface=preface)
