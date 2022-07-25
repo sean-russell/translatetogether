84d58858f1e4272b8f6bf4b17a70102d3b986aa3
Sean Russell <sean.russell@ucd.ie>
2022-07-25 14:20:17 +0100

    added

--- translator/__init__.py
+++ translator/__init__.py
@@ -13,8 +13,18 @@ from pylti1p3.lineitem import LineItem
 from pylti1p3.tool_config import ToolConfJsonFile
 from pylti1p3.registration import Registration
 
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        scheme = environ.get('HTTP_X_FORWARDED_PROTO')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
 
 app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 preface = ""
 
