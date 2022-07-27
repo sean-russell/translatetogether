007443d15b8a74c4c89dd19d8b0c8ef6ecaf8bae
Sean Russell <sean.russell@ucd.ie>
2022-07-27 11:23:57 +0100

    pushing to server

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # pytrans
+
+# Database Tables
+1. courses(<ins>iss, course_id</ins>, course_name)
+2. sections(<ins>*iss*,*course*, section_number</ins>, status)
+3. terms(<ins>id</ins>, term, *section*, *course_id* )
+
+4. participants(<ins>vle_user_id</ins>, email, vle_username, fullname, role, *iss*, *course*)
+5. assistants(<ins>id</ins>, email, *iss*, *course*)
+
+6. actions(<ins>id</ins>, *vle_user_id*, email, vle_username, *iss*, *course*, role, action_completed, action_time)
+
+7. trans_assignments(<ins>id</ins>, *vle_user_id*, term, *iss*, *course*, *section* )
+8. translations(<ins>id</ins>, *vle_user_id*, *trans_ass_id*, term, transterm, transdescription, *iss*, *course*, *section*)
+
+9. review_assignments(<ins>id</ins>, *reviewer_id*, *translator_id*, term, transterm, transdescription, *iss*, *course*, *section* )
+10. reviews(<ins>id</ins>, *rev_ass_id*, *reviewer_id*, *translator_id*, term, transterm, transdescription, review_score, review_comment, *iss*, *course*, *section* )
+
+11. vote_assignments(<ins>id</ins>, *voter_id*, *translator_id*, term, transterm, transdescription, *iss*, *course*, *section* )
+12. votes(<ins>id</ins>, *vote_ass_id*, *voter_id*, *translator_id*, term, transterm, transdescription, vote_score, *iss*, *course*, *section*)
--- init.sql
+++ init.sql
@@ -5,93 +5,168 @@ FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS translation;
 use translation;
 
-CREATE TABLE IF NOT EXISTS `status` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `course_id` varchar(255) NOT NULL,
-  `section` INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS `courses` (
+  `iss` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course_name` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  PRIMARY KEY (`iss`,`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `sections` (
+  `iss` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `section_number` INTEGER NOT NULL,
   `status` INTEGER NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`iss`,`course`, `section_number`),
+  CONSTRAINT fk_mrt_ot FOREIGN KEY (`iss`,`course`) REFERENCES `courses`(`iss`,`course_id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS `terms` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `term` varchar(255) NOT NULL,
+  `term` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `iss` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `section` INTEGER NOT NULL,
-  `course_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  CONSTRAINT fk_mrt_ot2 FOREIGN KEY (`iss`,`course`, `section`) REFERENCES `sections`(`iss`,`course`, `section_number`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE IF NOT EXISTS `students` (
+CREATE TABLE IF NOT EXISTS `participants` (
   `vle_user_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  `vle_username` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `vle_username` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
   `fullname` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `course_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  PRIMARY KEY (`vle_user_id`)
+  `role` varchar(255) CHARACTER SET utf8mb4 NOT NULL,	/* learner, instructor, admin*/
+  `iss` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  PRIMARY KEY (`vle_user_id`), UNIQUE(`email`), UNIQUE(`vle_username`),
+  CONSTRAINT fk_mrt_ot3 FOREIGN KEY (`iss`,`course`) REFERENCES `courses`(`iss`,`course_id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE IF NOT EXISTS `tas` (
+CREATE TABLE IF NOT EXISTS `assistants` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  `course_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  PRIMARY KEY (`id`)
+  `iss` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT fk_mrt_ot4 FOREIGN KEY (`iss`,`course`) REFERENCES `courses`(`iss`,`course_id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS `actions` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `vle_user_id` varchar(255) NOT NULL,
+  `vle_user_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `email` varchar(255) NOT NULL,
   `vle_username` varchar(255) NOT NULL,
-  `course_id` varchar(255) NOT NULL,
+  `iss` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `role` varchar(255) NOT NULL,
-  `actioncompleted` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
-  `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  `action_completed` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `action_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  CONSTRAINT fk_mrt_ot5 FOREIGN KEY (`iss`,`course`) REFERENCES `courses`(`iss`,`course_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`vle_user_id`) REFERENCES `participants`(`vle_user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE IF NOT EXISTS `assignments` (
+CREATE TABLE IF NOT EXISTS `trans_assignments` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `vle_id` varchar(255) NOT NULL,
-  `term_id` varchar(255) NOT NULL,
+  `vle_user_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `term` varchar(255) NOT NULL,
+  `iss` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `section` INTEGER NOT NULL,
-  `course_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`vle_user_id`) REFERENCES `participants`(`vle_user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_mrt_ot6 FOREIGN KEY (`iss`,`course`, `section`) REFERENCES `sections`(`iss`,`course`, `section_number`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS `translations` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `vle_id` varchar(255) NOT NULL,
-  `term` varchar(255) NOT NULL,
+  `trans_ass_id` bigint(20) NOT NULL,
+  `vle_user_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `term` varchar(255) NOT NULL, /*duplication of term in terms table*/
   `transterm` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `transdescription` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `transdescription` varchar(2048) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `iss` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `section` INTEGER NOT NULL,
-  `course_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`trans_ass_id`) REFERENCES `trans_assignments`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`vle_user_id`) REFERENCES `participants`(`vle_user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_mrt_ot7 FOREIGN KEY (`iss`,`course`, `section`) REFERENCES `sections`(`iss`,`course`, `section_number`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+
+CREATE TABLE IF NOT EXISTS `review_assignments` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `reviewer_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `translator_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `term` varchar(255) NOT NULL, /*duplication of term in terms table*/
+  `transterm` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,/*duplication of transterm in translations table*/
+  `transdescription` varchar(2048) CHARACTER SET utf8mb4 DEFAULT NULL, /*duplication of transdescription in translations table*/
+  `iss` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `section` INTEGER NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`reviewer_id`) REFERENCES `participants`(`vle_user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`translator_id`) REFERENCES `participants`(`vle_user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_mrt_ot8 FOREIGN KEY (`iss`,`course`, `section`) REFERENCES `sections`(`iss`,`course`, `section_number`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
 
 CREATE TABLE IF NOT EXISTS `reviews` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `reviewer_id` varchar(255) NOT NULL,
-  `translator_id` varchar(255) NOT NULL,
-  `transterm` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `transdescription` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `rev_ass_id` bigint(20) NOT NULL,
+  `reviewer_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `translator_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `term` varchar(255) NOT NULL, /*duplication of term in terms table*/
+  `transterm` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL, /*duplication of transterm in translations table*/
+  `transdescription` varchar(2048) CHARACTER SET utf8mb4 DEFAULT NULL, /*duplication of transdescription in translations table*/
   `review_score` INTEGER NOT NULL,
-  `review_comment` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `review_comment` varchar(2048) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `iss` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `section` INTEGER NOT NULL,
-  `course_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`reviewer_id`) REFERENCES `participants`(`vle_user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`translator_id`) REFERENCES `participants`(`vle_user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`rev_ass_id`) REFERENCES `review_assignments`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_mrt_ot9 FOREIGN KEY (`iss`,`course`, `section`) REFERENCES `sections`(`iss`,`course`, `section_number`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `vote_assignments` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `voter_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `translator_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `term` varchar(255) NOT NULL, /*duplication of term in terms table*/
+  `transterm` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,/*duplication of transterm in translations table*/
+  `transdescription` varchar(2048) CHARACTER SET utf8mb4 DEFAULT NULL, /*duplication of transdescription in translations table*/
+  `iss` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `section` INTEGER NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`voter_id`) REFERENCES `participants`(`vle_user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`translator_id`) REFERENCES `participants`(`vle_user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_mrt_ot10 FOREIGN KEY (`iss`,`course`, `section`) REFERENCES `sections`(`iss`,`course`, `section_number`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
 
 CREATE TABLE IF NOT EXISTS `votes` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `voter_id` varchar(255) NOT NULL,
-  `translator_id` varchar(255) NOT NULL,
-  `transterm` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `transdescription` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `vote_ass_id` bigint(20) NOT NULL,
+  `voter_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `translator_id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `term` varchar(255) NOT NULL, /*duplication of term in terms table*/
+  `transterm` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL, /*duplication of transterm in translations table*/
+  `transdescription` varchar(2048) CHARACTER SET utf8mb4 DEFAULT NULL, /*duplication of transdescription in translations table*/
   `vote_score` INTEGER NOT NULL,
+  `iss` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `course` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `section` INTEGER NOT NULL,
-  `course_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`voter_id`) REFERENCES `participants`(`vle_user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`translator_id`) REFERENCES `participants`(`vle_user_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`vote_ass_id`) REFERENCES `vote_assignments`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_mrt_ot11 FOREIGN KEY (`iss`,`course`, `section`) REFERENCES `sections`(`iss`,`course`, `section_number`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
 
