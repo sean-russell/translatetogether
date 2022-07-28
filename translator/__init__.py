1e74a1efd9a39e9fdd8030febc53424083724851
Sean Russell <sean.russell@ucd.ie>
2022-07-28 19:01:26 +0100

    pushing to server

--- translator/__init__.py
+++ translator/__init__.py
@@ -263,7 +263,7 @@ def assign_term(data: Dict) -> None:
 @app.route('/tas/add/', methods=['POST'])
 def add_teaching_assistants():
     data = json.loads(request.form['datajson'])
-    ta_emails = [ a.trim() for a in request.form['tas'].split(',') if a.trim() != '' ] 
+    ta_emails = [ a.strip() for a in request.form['tas'].split(',') if a.strip() != '' ] 
     dbstuff.add_tas_to_course(data['iss'], data['course'], ta_emails)
     data['sections'] = dbstuff.get_sections_for_course(data['iss'], data['course'])
     data['tas'] = dbstuff.get_ta_details_for_course(data['iss'], data['course'])
