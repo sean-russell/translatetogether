03820ead67405b47b8ada4a2489c244cfc9a1e73
Sean Russell <sean.russell@ucd.ie>
2022-08-09 20:59:02 +0100

    too tired to think

--- translator/__init__.py
+++ translator/__init__.py
@@ -184,6 +184,7 @@ def main_page():
                 if data['section']['status'] in (STATUS_VOTES_ASSIGNED, convert_status(STATUS_VOTES_ASSIGNED)):
                     data['candidates']: Dict[str,List[Dict[str,str]]] = {}
                     candidates = dbstuff.get_votes_for_student_in_section(data['id'], data['iss'], data['course'], data['section_num'])
+                    
                     candidates = [ {
                             "vote_assign_id": c.vote_assign_id,
                             "v_id": c.v_id,
@@ -398,10 +399,12 @@ def start_voting():
     term_assignments = dbstuff.get_trans_assignments_for_section_of_course(iss, course, section_num)
     print("vote candidates", vote_candidates)
     print("term assignments", term_assignments)
+    term_ids = []
     for term, ass_list in term_assignments.items():
         for ass in ass_list:
             for vc in vote_candidates:
-                if vc.term != term:
+                if vc.term != term and vc.term_id not in term_ids:
+                    term_ids.append(vc.term_id)
                     dbstuff.assign_vote_to_student(ass.id, vc, iss, course, section_num)
     dbstuff.set_status_of_section(data['iss'], data['course'], section_num, STATUS_VOTES_ASSIGNED)
     data['section'] = dbstuff.get_section_for_course(data['iss'], data['course'], section_num)
